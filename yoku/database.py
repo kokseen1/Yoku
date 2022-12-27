4a926d4470d6128430be0b1a96aa284b22b148bc
kose <53458630+kokseen1@users.noreply.github.com>
2022-12-27 20:11:07 +0800

    fix: use lock for tinydb race condition

--- yoku/database.py
+++ yoku/database.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from tinydb import TinyDB, Query
+from threading import Lock
 
 from yoku.consts import KEY_ITEM_ID, KEY_CHAT_ID
 from yoku.scrape import search
@@ -11,6 +12,8 @@ TABLE_QUERIES = "queries"
 KEY_QUERY = "query"
 
 
+lock = Lock()
+
 db = TinyDB(str(DB_PATH))
 
 
@@ -19,14 +22,15 @@ def add_query(chat_id, query):
     Add a user query to the database
     """
 
-    queries_table = db.table(TABLE_QUERIES)
-    Item = Query()
+    with lock:
+        queries_table = db.table(TABLE_QUERIES)
+        Item = Query()
 
-    # Skip if already exists
-    if queries_table.search((Item[KEY_QUERY] == query) & (Item[KEY_CHAT_ID] == chat_id)):
-        return
+        # Skip if already exists
+        if queries_table.search((Item[KEY_QUERY] == query) & (Item[KEY_CHAT_ID] == chat_id)):
+            return
 
-    queries_table.insert({KEY_QUERY: query, KEY_CHAT_ID: chat_id})
+        queries_table.insert({KEY_QUERY: query, KEY_CHAT_ID: chat_id})
 
 
 def remove_query(chat_id, query):
@@ -34,20 +38,22 @@ def remove_query(chat_id, query):
     Remove a user query from the database
     """
 
-    queries_table = db.table(TABLE_QUERIES)
-    Item = Query()
+    with lock:
+        queries_table = db.table(TABLE_QUERIES)
+        Item = Query()
 
-    queries_table.remove((Item[KEY_QUERY] == query)
-                         & (Item[KEY_CHAT_ID] == chat_id))
+        queries_table.remove((Item[KEY_QUERY] == query)
+                            & (Item[KEY_CHAT_ID] == chat_id))
 
 
 def list_queries(chat_id):
-    queries_table = db.table(TABLE_QUERIES)
-    Item = Query()
+    with lock:
+        queries_table = db.table(TABLE_QUERIES)
+        Item = Query()
 
-    queries = queries_table.search(Item[KEY_CHAT_ID] == chat_id)
+        queries = queries_table.search(Item[KEY_CHAT_ID] == chat_id)
 
-    return [x[KEY_QUERY] for x in queries]
+        return [x[KEY_QUERY] for x in queries]
 
 
 def search_and_notify(bot):
@@ -56,20 +62,21 @@ def search_and_notify(bot):
     Cache results and notify users of new results
     """
 
-    queries_table = db.table(TABLE_QUERIES)
+    with lock:
+        queries_table = db.table(TABLE_QUERIES)
 
-    for row in queries_table:
-        chat_id = row[KEY_CHAT_ID]
-        query = row[KEY_QUERY]
+        for row in queries_table:
+            chat_id = row[KEY_CHAT_ID]
+            query = row[KEY_QUERY]
 
-        results = search(query)
-        cache_table = db.table(chat_id)
+            results = search(query)
+            cache_table = db.table(chat_id)
 
-        for result in results:
-            item_id = result[KEY_ITEM_ID]
+            for result in results:
+                item_id = result[KEY_ITEM_ID]
 
-            Item = Query()
-            if not cache_table.search(Item[KEY_ITEM_ID] == item_id):
-                notify(chat_id, result, bot)
+                Item = Query()
+                if not cache_table.search(Item[KEY_ITEM_ID] == item_id):
+                    notify(chat_id, result, bot)
 
-            cache_table.upsert(result, Item[KEY_ITEM_ID] == item_id)
+                cache_table.upsert(result, Item[KEY_ITEM_ID] == item_id)
