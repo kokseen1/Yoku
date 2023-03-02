7397ab8d7be0999c6a33bc5a4455f78b91fd4d4f
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2023-03-02 20:51:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="Yoku",
-    version="1.0.7",
+    version="1.0.8",
     packages=["yoku"],
     description="A minimal Yahoo! Auctions scraper.",
     long_description=long_description,
